ec0e2d72b4f4a372a110ca4e23cbbb6678ae6461
Dan Nguyen <dansonguyen@gmail.com>
2018-05-31 15:14:08 -0700

    hi

--- scripts/wrangle.py
+++ scripts/wrangle.py
@@ -2,12 +2,67 @@ import os.path
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../scripts")))
 
-
 from bootstrap import DATA_PATH as RAW_DATA_PATH
+import csv
+from pathlib import Path
+
+DATA_DIR = Path('static', 'data')
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+DATA_PATH = Path(DATA_DIR, 'crimes.csv')
+
+
+WRANGLED_HEADERS = [
+    'category',
+    'sub_category',
+    'arrest_made',
+    'latitude',
+    'longitude',
+]
 
 def read_raw_data():
     txt = RAW_DATA_PATH.read_text()
-    return txt
+    lines = txt.splitlines()
+    data = list(csv.DictReader(lines))
+
+    return data
+
+def filter_crimes(records):
+    data = []
+    for row in records:
+        if 'GUN' in row[' SECONDARY DESCRIPTION']:
+            k = __fixrow(row)
+            data.append(k)
+    return data
+
+def __fixrow(row):
+    d = {}
+    d['sub_category'] = row[' SECONDARY DESCRIPTION']
+    d['category'] = row[' PRIMARY DESCRIPTION']
+    d['arrest_made'] = row['ARREST']
+    d['latitude'] = row['LATITUDE']
+    d['longitude'] = row['LONGITUDE']
+    return d
+
+
+def wrangle():
+    raw_records = read_raw_data()
+    print("Raw data has", len(raw_records), "records")
+
+    filtered_records = filter_crimes(raw_records)
+    print("Filtered data has", len(filtered_records), "records")
+
+    # headers is just the list of key names for any given dict
+    # in the records
+    headers = filtered_records[0].keys()
+
+
+    with open(DATA_PATH, 'w') as f:
+        w = csv.DictWriter(f, fieldnames=WRANGLED_HEADERS)
+        w.writeheader()
+        w.writerows(filtered_records)
+
+
+
 
 
 
@@ -15,5 +70,4 @@ def read_raw_data():
 
 
 if __name__ == "__main__":
-    txt = read_raw_data()
-    print(len(txt), 'chars')
+    wrangle()
